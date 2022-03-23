626b61bd0868108361166c67c49dda2d0dedc81e
Jonathan Lopez <122jlopez@frhsd.com>
2022-03-23 11:00:03 -0400

    Finished step 4 and 5 by displaying movie information and image.

--- site.js
+++ site.js
@@ -34,7 +34,7 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            title:"Top 8 Movies",
+            title:"IMDB + Jonathan's Top 8 Movies",
             owner:"Jonathan Lopez",
             github:"https://122jlopez.github.io/Lopez-P3/",
       }
