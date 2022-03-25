30dcf8f2dacd144b8b269afcb5ad2f6af162f2bc
Jonathan Lopez <122jlopez@frhsd.com>
2022-03-25 08:36:49 -0400

    finished milestone 3 and finished some functions

--- site.js
+++ site.js
@@ -41,6 +41,12 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            function getMonthText(dateArray){
+
+            },
+            function like(index) {
+
+            },
       }
 })
 
